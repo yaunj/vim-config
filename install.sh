32d06dccf13bfbe46a537d627d72ec87c931e8b6
Rune Hammersland <rune@hammersland.net>
2012-01-13 01:12:34 +0100

    Install script for Vim files.
    Creates symlinks in $HOME for vimrc and vim/.

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd $(dirname $0)
+echo -n "Updating git modules "
+git submodule init                         >/dev/null 2>&1 && echo -n ". "
+git submodule update                       >/dev/null 2>&1 && echo -n ". "
+git submodule foreach git submodule init   >/dev/null 2>&1 && echo -n ". "
+git submodule foreach git submodule update >/dev/null 2>&1 && echo -n ". "
+echo "done"
+
+for f in *; do
+    [[ "${f}" = "${0}" ]] && continue
+    [[ "${f:0:6}" = "README" ]] && continue
+    link=${HOME}/.${f}
+    this=${PWD}/${f}
+
+    if [[ -e "${link}" ]]; then
+        if [[ -L "${link}" ]]; then
+            end=$(python -c "import os; print os.path.realpath('${link}');")
+
+            if [[ "${end}" != "${this}" ]]; then
+                echo "[FAIL] ${link} points elsewhere. Fix it yourself." >&2
+            fi
+        else
+            echo "[FAIL] .${f} exists in \$HOME. Symlink it yourself." >&2
+        fi
+    else
+        echo ln -s "${this}" "${link}"
+        echo "[ OK ] Created $link" >&2
+    fi
+done
